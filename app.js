41dfbc28d7e6c655e29865a9e6ae1f68710bc94c
Chaima <93583596+ChaimaElMoussaoui@users.noreply.github.com>
2024-12-04 22:21:08 +0100

    last changes

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ function addTodoItem() {
     saveTodos();
     updateTodoList();
 
-    todoInput.value = ''; 
+    todoInput.value = '';
 }
 
 function updateTodoList() {
     const todoList = document.getElementById('todo-list');
-    todoList.innerHTML = ''; 
+    todoList.innerHTML = '';
 
     allTodos.forEach((todo, index) => {
         if (todo.category === currentCategory) {
@@ -165,20 +165,20 @@ let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
 }
 
-if(darkmode === "active") enableDarkmode()
+if (darkmode === "active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
 
 
