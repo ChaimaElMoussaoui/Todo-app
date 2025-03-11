6df851ddcedab18055a078c77d433636efc232fc
Chaima <93583596+ChaimaElMoussaoui@users.noreply.github.com>
2025-03-11 11:34:35 +0100

    Update 2

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
             draggable.classList.remove('dragging');
         });
     });
+    
+containers.forEach(container => {
+        container.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(container, e.clientY);
+            const draggable = document.querySelector('.dragging');
+            if (afterElement == null) {
+                container.appendChild(draggable);
+            } else {
+                container.insertBefore(draggable, afterElement);
+            }
+        });
+    });
+
